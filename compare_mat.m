fdfefc7c01ad7eb8e1ccf9ccaf9c92d595741c04
hmph <anurag.arnab@gmail.com>
2016-07-06 10:07:48 +0100

    compare_mat: fix

--- compare_mat.m
+++ compare_mat.m
@@ -1,8 +1,11 @@
+% Returns how many elements are different in mat1 and mat2
+% Returns -1 if the size of the matrices being compared are not the same
 function [diff] = compare_mat (mat1, mat2)
 
     if (size(mat1) ~= size(mat2) )
        diff = -1; 
+       return;
     end
     
-    diff = sum(abs(mat1(:) ~= mat2(:)));
+    diff = sum( mat1(:) ~= mat2(:) );
 end
