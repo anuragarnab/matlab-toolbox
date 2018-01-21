da829092f3e586d1501620427a1e10a1a78e870b
hmph <anurag.arnab@gmail.com>
2018-01-21 21:33:50 +0000

    add optional label adjustment

--- figures/superimpose_image_label.m
+++ figures/superimpose_image_label.m
@@ -1,6 +1,7 @@
-function [output] = superimpose_image_label(image_name, label_name, alpha, colour_map)
+function [output] = superimpose_image_label(image_name, label_name, alpha, colour_map, label_adjustment)
 
     if (nargin < 4) colour_map = -1; end;
+    if (nargin < 5) label_adjustment = 0; end;
 
     im = imread(image_name);
     [label, map] = imread(label_name);
@@ -9,6 +10,7 @@ function [output] = superimpose_image_label(image_name, label_name, alpha, colou
        map = colour_map; 
     end
     
+    label = label + label_adjustment;
     label_im = ind2rgb(label, map) .* 255;
     
     output = blend_images(im, label_im, alpha);
