894ec4c0d9a23c191f64d3866d981e8ab170e2f7
hmph <anurag.arnab@gmail.com>
2018-01-21 21:30:51 +0000

    get list of scatter plot markers to iterate over

---
+++ figures/get_scatter_marker_types.m
@@ -0,0 +1,6 @@
+function [all_marker_types, marker_fills] = get_scatter_marker_types()
+
+	all_marker_types = {'o','+','*','.','x','square','diamond','^','v','>','<','pentagram','hexagram'};
+	marker_fills = {'o', 'square','diamond','^','v','>','<','pentagram','hexagram'};
+
+end
